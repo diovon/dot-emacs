34a391fe779dc445ca9f5c29f725ef4160b74875
andreas <mail@andreas-voit.at>
2020-01-14 20:29:31 +0100

    erster Commit

---
+++ init.el
@@ -0,0 +1,23 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t)
+ '(package-selected-packages '(auctex yasnippet vlf use-package projectile jedi counsel)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; Line wrap
+(setq org-startup-indented t)
+
+;; set start of week to Monday (not sunday)
+(setq-default calendar-week-start-day 1)
+
+;; Maximize frame window
+(toggle-frame-maximized)
+
